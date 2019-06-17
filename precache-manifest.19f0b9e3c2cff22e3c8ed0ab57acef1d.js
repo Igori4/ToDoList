d88d6a3e0dfa202e377cb4586f69a86187239dde
Igori4 <igorpilyak@gmail.com>
2019-06-17 23:20:41 +0300

    Updates

---
+++ precache-manifest.19f0b9e3c2cff22e3c8ed0ab57acef1d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c218c51b43ad9ef3543e862caf4bf670",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "34306836d458700030c8",
+    "url": "/ToDoList/static/css/main.9f30e42d.chunk.css"
+  },
+  {
+    "revision": "fbe5485156ae5d448141",
+    "url": "/ToDoList/static/js/2.a23af5b4.chunk.js"
+  },
+  {
+    "revision": "34306836d458700030c8",
+    "url": "/ToDoList/static/js/main.100e9004.chunk.js"
+  },
+  {
+    "revision": "6a71c43a298729cf07dd",
+    "url": "/ToDoList/static/js/runtime~main.67fadf76.js"
+  }
+]);
